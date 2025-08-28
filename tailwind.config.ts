90f5de62a05feea1c9b02ad491796e372a460e57
Andile Sithole <Andile.Sithole@loadshednomodev.onmicrosoft.com>
2025-08-28 23:56:22 +0200

    feat: Update project metadata and improve code formatting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -115,19 +115,19 @@ export default {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
-        "slideDownAndFade": {
+        slideDownAndFade: {
           from: { opacity: "0", transform: "translateY(-2px)" },
           to: { opacity: "1", transform: "translateY(0)" },
         },
-        "slideLeftAndFade": {
+        slideLeftAndFade: {
           from: { opacity: "0", transform: "translateX(2px)" },
           to: { opacity: "1", transform: "translateX(0)" },
         },
-        "slideUpAndFade": {
+        slideUpAndFade: {
           from: { opacity: "0", transform: "translateY(2px)" },
           to: { opacity: "1", transform: "translateY(0)" },
         },
-        "slideRightAndFade": {
+        slideRightAndFade: {
           from: { opacity: "0", transform: "translateX(-2px)" },
           to: { opacity: "1", transform: "translateX(0)" },
         },
@@ -135,10 +135,13 @@ export default {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "slideDownAndFade": "slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
-        "slideLeftAndFade": "slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
-        "slideUpAndFade": "slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
-        "slideRightAndFade": "slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideDownAndFade:
+          "slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideLeftAndFade:
+          "slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideUpAndFade: "slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideRightAndFade:
+          "slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
       },
     },
   },
